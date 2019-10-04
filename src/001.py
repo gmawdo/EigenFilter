a388a1ed8bac01d0a82205d5af50141c6c1abb41
Gary Mawdsley <79173+gmawdo@users.noreply.github.com>
2019-10-04 12:56:42 +0200

    Structure of the project

--- src/001.py
+++ src/001.py
@@ -3,7 +3,7 @@ from redhawkmaster import rh_io
 from redhawkmaster.las_modules import las_range
 
 job = '001'
-f000 = rh_io.las_input('ILIJA_FlightlineTest_Tile000.las', mode='r')
+f000 = rh_io.las_input('TestArea.las', mode='r')
 
 point_id = np.arange(len(f000))
 
@@ -13,6 +13,6 @@ mask = las_range(f000.classification,
                  reverse=True,
                  point_id_mask=point_id)
 
-f001 = rh_io.las_output('ILIJA_FlightlineTest_Tile000_job001.las', f000, mask)
+f001 = rh_io.las_output('TestArea_job001.las', f000, mask)
 
 f001.close()
