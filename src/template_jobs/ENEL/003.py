43707dfd1cc04b4a46f50f76e4b9c5f1909e6ec1
Gary Mawdsley <79173+gmawdo@users.noreply.github.com>
2019-11-15 16:54:26 +0100

    Added 003 job. Fixes #8.

---
+++ src/template_jobs/ENEL/003.py
@@ -0,0 +1,36 @@
+import pathmagic
+
+from redhawkmaster.las_modules import las_range
+from redhawkmaster.rh_io import las_input, las_output
+
+assert pathmagic
+
+input_file = 'T000_ground.las'
+# Name of the output file
+output_file1 = 'T000_ground_points.las'
+
+output_file2 = 'T000_non_ground_points.las'
+
+# Read the input file
+infile = las_input(input_file,
+                   mode='r')
+
+point_id_ground = las_range(dimension=infile.classification,
+                            start=2, end=3,
+                            reverse=False,
+                            point_id_mask=infile.slpid)
+
+point_id_non_ground = las_range(dimension=infile.classification,
+                                start=2, end=3,
+                                reverse=True,
+                                point_id_mask=infile.slpid)
+
+outfile1 = las_output(output_file1,
+                      infile,
+                      mask=point_id_ground)
+outfile1.close()
+
+outfile2 = las_output(output_file2,
+                      infile,
+                      mask=point_id_non_ground)
+outfile2.close()
