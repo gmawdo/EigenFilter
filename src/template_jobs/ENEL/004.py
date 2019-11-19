40515d709bfe776a249c6c79be826a0777be32bf
Gary Mawdsley <79173+gmawdo@users.noreply.github.com>
2019-11-19 12:18:58 +0100

    Added default paramters in 004. Fixes #9.

--- src/template_jobs/ENEL/004.py
+++ src/template_jobs/ENEL/004.py
@@ -10,4 +10,10 @@ input_file = 'T000_non_ground_points.las'
 # Name of the output file
 output_file = 'T000_attr.las'
 
-add_attributes(input_file, output_file)
+add_attributes(input_file,
+               output_file,
+               time_intervals=10,
+               k=range(4, 50),
+               radius=0.5,
+               virtual_speed=0,
+               voxel_size=0)
