97fa3898307b31fbd5cf91ecd2896e2eff53250e
Gary Mawdsley <79173+gmawdo@users.noreply.github.com>
2019-11-20 12:16:31 +0100

    Added job 008. Corridor 2D tool. Fixes #20.

--- src/template_jobs/ENEL/008.py
+++ src/template_jobs/ENEL/008.py
@@ -2,14 +2,14 @@ import pathmagic
 
 from redhawkmaster import rh_io
 from redhawkmaster.rh_big_guns import pdal_smrf
-from redhawkmaster.rh_dean import bbox, bbox_rectangle
+from redhawkmaster.rh_dean import bbox, bbox_rectangle, corridor_2d
 from redhawkmaster.rh_io import las_input
 
 assert pathmagic
 
 input_file = 'T000_bbox_in.las'
 # Name of the output file
-output_file = 'T000_bbox_out_tool1.las'
+output_file = 'T000_bbox_out_tool2.las'
 
 infile = rh_io.las_input(input_file, mode='r')
 outfile = rh_io.las_output(output_file, infile)
@@ -19,7 +19,10 @@ classn = bbox_rectangle(infile,
                         classification_in=2,
                         accuracy=1000)
 
-print(classn)
+corridor_2d(outfile,
+            distance=1,
+            angle_th=0.2)
+
 outfile.close()
 
 # bbox(input_file, output_file)
