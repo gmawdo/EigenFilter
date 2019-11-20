1249535617c4ebc9b583f5875c68938766441c81
Gary Mawdsley <79173+gmawdo@users.noreply.github.com>
2019-11-20 13:51:39 +0100

    Added 008. Recover tool. Fixes #22.

--- src/template_jobs/ENEL/008.py
+++ src/template_jobs/ENEL/008.py
@@ -2,24 +2,26 @@ import pathmagic
 
 from redhawkmaster import rh_io
 from redhawkmaster.rh_big_guns import pdal_smrf
-from redhawkmaster.rh_dean import bbox, bbox_rectangle
+from redhawkmaster.rh_dean import bbox, bbox_rectangle, recover_un
 from redhawkmaster.rh_io import las_input
 
 assert pathmagic
 
-input_file = 'T000_bbox_in.las'
+input_file = 'T000_bbox_out_tool3.las'
 # Name of the output file
-output_file = 'T000_bbox_out_tool1.las'
+output_file = 'T000_bbox_out_tool4.las'
 
 infile = rh_io.las_input(input_file, mode='r')
 outfile = rh_io.las_output(output_file, infile)
 
-classn = bbox_rectangle(infile,
-                        outfile,
-                        classification_in=2,
-                        accuracy=1000)
+# classn = bbox_rectangle(infile,
+#                         outfile,
+#                         classification_in=2,
+#                         accuracy=1000)
 
-print(classn)
+recover_un(outfile,
+           classification_un=0,
+           classification_pyl=5)
 outfile.close()
 
 # bbox(input_file, output_file)
