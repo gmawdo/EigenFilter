b3a046d9ba524bcd5efbab969c2cfbd4558a6dee
Gary Mawdsley <79173+gmawdo@users.noreply.github.com>
2019-12-13 16:49:11 +0100

    Seq. running of the files. Fixes #59.

--- src/manager/execute.py
+++ src/manager/execute.py
@@ -8,14 +8,14 @@ import shlex
 
 from multiprocessing.dummy import Pool as ThreadPool
 
+from manager.zmerge import las_zip, merge_job
 from redhawkmaster.rh_big_guns import rh_tiling_gps_equal_filesize
 
 assert pathmagic
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--code', help='Location where is your redhawk code.', required=True)
-parser.add_argument('-b', '--bigfile', help='Location where the big file is it.', required=True)
-parser.add_argument('-d', '--data', help='Location where is the data you need to run.', required=True)
+# parser.add_argument('-d', '--data', help='Location where is the data you need to run.', required=True)
 parser.add_argument('-r', '--results', help='Location where the result data to be.', required=True)
 parser.add_argument('-f', '--flow', help='Location where the flow of the jobs is.', required=True)
 parser.add_argument('-t', '--template', help='Which template jobs to use.', required=True)
@@ -120,7 +120,7 @@ def run_process(cmd):
         subprocess.call(cm, shell=True)
 
 
-def parallel(args):
+def parallel(args, big_file):
     """
     Function that is making the commands that needs to be runned into the docker containers.
     :param args: arguments from the output command.
@@ -132,11 +132,14 @@ def parallel(args):
     if not os.path.exists(args.results):
         os.mkdir(args.results)
 
+    if not os.path.exists(args.results + '/' + big_file):
+        os.mkdir(args.results + '/' + big_file)
+
     # If the logs folder is not there make if it is delete everything inside
-    if not os.path.exists(args.results + '/logs'):
-        os.mkdir(args.results + '/logs')
+    if not os.path.exists(args.results + '/' + big_file + '/logs'):
+        os.mkdir(args.results + '/' + big_file + '/logs')
     else:
-        os.system('rm '+args.results + '/logs/*')
+        os.system('rm ' + args.results + '/' + big_file + '/logs/*')
 
     # Count how much processes we have
     process_count = 0
@@ -145,11 +148,11 @@ def parallel(args):
     with open(args.flow) as f:
         data = f.readlines()
         # Make the folders for each tile and get the locations of the file
-        for fil in os.listdir(args.data):
+        for fil in os.listdir(args.results + '/200_TILES/' + big_file + '/'):
             result = []
             fil = fil.split('.')[0]
-            if not os.path.exists(args.results + '/' + fil):
-                os.mkdir(args.results + '/' + fil)
+            if not os.path.exists(args.results + '/' + big_file + '/' + fil):
+                os.mkdir(args.results + '/' + big_file + '/' + fil)
             count = 0
             process_count += 1
 
@@ -166,32 +169,35 @@ def parallel(args):
 
                 # Construct the command
                 command = 'echo "=====   Job no.{} for tile {} start   ===== $(date)"  >> {}/logs/log_{}.out && ' \
-                          'docker run -it --rm -v {}:/code -v {}:/data -v {}:/results ' \
+                          'docker run --rm -v {}:/code -v {}:/data -v {}:/results ' \
                           'redhawk python3 /code/template_jobs/{}/{}.py {} {} >> {}/logs/log_{}.out && ' \
                           'echo "=====   Job no.{} for tile {} end     ===== $(date)" >> ' \
                           '{}/logs/log_{}.out '
-                command = command.format(line[-1].strip(), fil, args.results, fil, args.code, args.data, args.results,
-                                         args.template, line[-1].strip(), in_files, out_files, args.results, fil,
-                                         line[-1].strip(), fil, args.results, fil)
+                command = command.format(line[-1].strip(), fil, args.results + '/' + big_file, fil, args.code,
+                                         args.results + '/200_TILES/' + big_file + '/', args.results + '/' + big_file,
+                                         args.template, line[-1].strip(), in_files, out_files,
+                                         args.results + '/' + big_file, fil,
+                                         line[-1].strip(), fil, args.results + '/' + big_file, fil)
 
                 if line[-1].strip() == 'cleanup':
                     command = 'echo "=====   {} for tile {} start   ===== $(date)"  >> {}/logs/log_{}.out && ' \
-                              'docker run -it --rm -v {}:/data -v {}:/results ' \
+                              'docker run --rm -v {}:/data -v {}:/results ' \
                               'redhawk rm {} >> {}/logs/log_{}.out && ' \
                               'echo "=====   {} for tile {} end     ===== $(date)" >> ' \
-                              '{}/logs/log_{}.out '.format(line[-1].strip(), fil, args.results, fil, args.data,
-                                                           args.results,
-                                                           in_files, args.results, fil, line[-1].strip(), fil,
-                                                           args.results, fil)
+                              '{}/logs/log_{}.out '.format(line[-1].strip(), fil, args.results + '/' + big_file, fil,
+                                                           args.results + '/200_TILES/' + big_file + '/',
+                                                           args.results + '/' + big_file,
+                                                           in_files, args.results + '/' + big_file, fil,
+                                                           line[-1].strip(), fil,
+                                                           args.results + '/' + big_file, fil)
 
                 # command = 'echo "=====   Job no.'+line[-1].strip()+' for tile '+fil+' start   ===== $(date)"  >>'\
-                #           + args.results + '/logs/log' + str(process_count-1).zfill(3) + '.out  &&' \
-                #           ' docker run -it --rm -v ' + args.code + ':/code -v ' + args.data + ':/data ' \
-                #           '-v ' + args.results + ':/results redhawk python3 /code/template_jobs/' + \
-                #           args.template + '/' + line[-1].strip() + '.py ' + in_files + out_files + \
-                #           ' >> ' + args.results + '/logs/log' + str(process_count-1).zfill(3) + '.out '\
-                #           ' && echo "=====   Job no.'+line[-1].strip()+' for tile '+fil+' end     ===== $(date)" >> '\
-                #           + args.results + '/logs/log' + str(process_count-1).zfill(3) + '.out '
+                # + args.results + '/logs/log' + str(process_count-1).zfill(3) + '.out  &&' \ ' docker run -it --rm
+                # -v ' + args.code + ':/code -v ' + args.results + '/200_TILES/' + ':/data ' \ '-v ' + args.results +
+                # ':/results redhawk python3 /code/template_jobs/' + \ args.template + '/' + line[-1].strip() + '.py
+                # ' + in_files + out_files + \ ' >> ' + args.results + '/logs/log' + str(process_count-1).zfill(3) +
+                # '.out '\ ' && echo "=====   Job no.'+line[-1].strip()+' for tile '+fil+' end     ===== $(date)" >>
+                # '\ + args.results + '/logs/log' + str(process_count-1).zfill(3) + '.out '
 
                 result.append(command)
 
@@ -204,17 +210,77 @@ def parallel(args):
     pool.close()
 
 
+def get_last_job():
+    fileHandle = open(args.flow, 'r')
+    lineList = fileHandle.readlines()
+    if lineList[-1].split(',')[-1] == 'cleanup':
+        if lineList[-2].strip().split(',')[-1] in lineList[-2].strip().split(',')[-2]:
+            return lineList[-2].strip().split(',')[-2]
+    if lineList[-1].strip().split(',')[-1] in lineList[-1].strip().split(',')[-2]:
+        return lineList[-1].strip().split(',')[-2]
+    return lineList[-1].strip().split(',')[-1]
+
+
 if __name__ == '__main__':
-    start = time.time()
 
-    if args.bigfile != 'None':
-        print("===== Tiling Start =====")
-        rh_tiling_gps_equal_filesize(args.bigfile, args.data + '/', filesize=float(args.mbpt))
-        print("===== Tiling End   =====")
 
-    print("===== Processing Start =====")
-    parallel(args)
-    print("===== Processing End   =====")
+    if not os.path.exists(args.results):
+        os.mkdir(args.results)
 
-    end = time.time()
-    print("Time: " + str(end - start))
+    if not os.path.exists(args.results + '/000_RECEIVED_DATA_LAZ'):
+        os.mkdir(args.results + '/000_RECEIVED_DATA_LAZ')
+
+    if not os.path.exists(args.results + '/001_RECEIVED_DATA_LAZ_COMPLETE'):
+        os.mkdir(args.results + '/001_RECEIVED_DATA_LAZ_COMPLETE')
+
+    if not os.path.exists(args.results + '/100_BIG_FILES_LAS'):
+        os.mkdir(args.results + '/100_BIG_FILES_LAS')
+
+    if not os.path.exists(args.results + '/200_TILES'):
+        os.mkdir(args.results + '/200_TILES')
+
+    if not os.path.exists(args.results + '/300_PRODUCTS'):
+        os.mkdir(args.results + '/300_PRODUCTS')
+
+    while 1:
+        time.sleep(5)
+        start = time.time()
+        name_list = os.listdir(args.results + '/000_RECEIVED_DATA_LAZ/')
+        full_list = [os.path.join(args.results + '/000_RECEIVED_DATA_LAZ/', i) for i in name_list]
+        time_sorted_list = sorted(full_list, key=os.path.getmtime)
+        sorted_filename_list = [os.path.basename(i) for i in time_sorted_list]
+        # print(sorted_filename_list[0].split('.')[0])
+
+        if sorted_filename_list:
+            big_file = sorted_filename_list[0].split('.')[0]
+
+            if not os.path.exists(args.results + '/200_TILES/' + big_file):
+                os.mkdir(args.results + '/200_TILES/' + big_file)
+
+            print("===== Unlaz Start =====")
+            las_zip(laz_location=args.results + '/000_RECEIVED_DATA_LAZ',
+                    las_location=args.results + '/100_BIG_FILES_LAS',
+                    move_location=args.results + '/001_RECEIVED_DATA_LAZ_COMPLETE',
+                    tile_name=big_file,
+                    unzip=True)
+            print("===== Unlaz End   =====")
+            # if args.bigfile != 'None':
+            print("===== Tiling Start =====")
+            rh_tiling_gps_equal_filesize(args.results + '/100_BIG_FILES_LAS/' + big_file + '.las',
+                                         args.results + '/200_TILES/' + big_file + '/', filesize=float(args.mbpt))
+            print("===== Tiling End   =====")
+
+            print("===== Processing Start =====")
+            parallel(args, big_file)
+            print("===== Processing End   =====")
+
+            print("===== Merge Start =====")
+            print(get_last_job())
+            merge_job(las_tile_location=args.results + '/200_TILES/' + big_file + '/',
+                      tile_results=args.results + '/' + big_file+'/',
+                      out_location=args.results + '/300_PRODUCTS/',
+                      job_number=get_last_job(),
+                      compressed=True)
+            print("===== Merge End   =====")
+        end = time.time()
+        print("Time: " + str(end - start))
