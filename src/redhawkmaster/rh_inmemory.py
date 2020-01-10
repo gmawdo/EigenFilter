60e7f782a50a7f8f84c2d4e45e4ac23509166650
Gary Mawdsley <79173+gmawdo@users.noreply.github.com>
2020-01-10 12:01:20 +0000

    FINALLY have something working which doesn't go infinite

--- src/redhawkmaster/rh_inmemory.py
+++ src/redhawkmaster/rh_inmemory.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def nd_array_setter(key, data_type):
     """
-    This function is used in point_cloud_type to define the property corresponding to each datatype.
+    This function is used in array_manager_type to define the property corresponding to each datatype.
     It defines what happens when users set the property for each dimension.
     @param key: name of dimension
     @param data_type: numpy data type for that dimension
@@ -24,7 +24,7 @@ def nd_array_setter(key, data_type):
 
 def nd_array_getter(key):
     """
-    This function is used in point_cloud_type to define the property corresponding to each datatype.
+    This function is used in array_manager_type to define the property corresponding to each datatype.
     It accesses the hidden variable so that users can evaluate the property for each dimension.
     @param key: name of dimension
     @return: the fget function for this data_type, for the property class
@@ -37,33 +37,14 @@ def nd_array_getter(key):
     return getter
 
 
-def nd_array_deleter(key):
+def array_manager_type(name, data_types):
     """
-    This function is used in point_cloud_type to delete the property corresponding to each datatype.
-    It accesses deletes the hidden variable so that users can evaluate the property for each dimension.
-    @param key: name of dimension
-    @return: the fdel function for this data_type, for the property class
-    """
-
-    def deleter(self):
-        # simply extract the value from the hidden variable
-        delattr(self, "__" + key)
-        delattr(type(self), key)
-        del self.data_types[key]
-
-    return deleter
-
-
-def point_cloud_type(name, data_types):
-    """
-    This function produces the type of a file with a given dictionary of data types
+    This function produces the type of an array manager with a given dictionary of data types
 
     @param name: Name of class. See documentation for type.
     @param data_types: Dictionary of data types
     e.g. data_types = {"x": np.float64, "y": np.float64, "z": np.float64, "intensity": np.int8}
-    @return: a type which takes a parameters two parameters "shape" (a positive integer) and "user_info" (anything)
-    to initialise. Note that the values for the data parameters can be set later. Following from the above example,
-    one can set inFile.x, inFile.y, inFile.z, and inFile.intensity once an object inFile is initialised.
+    @return: a type whose objects are initialised shape. Note that the values for the data parameters can be set later.
     """
 
     def __init__(self, shape):
@@ -72,28 +53,80 @@ def point_cloud_type(name, data_types):
     def add_dimension(self, key, data_type):
         new_data_types = self.data_types
         new_data_types[key] = data_type
-        new_point_cloud_type = point_cloud_type(name=name+str(new_data_types),
-                                                data_types=new_data_types)
-        return new_point_cloud_type
+        new_array_manager_type = array_manager_type(name=name,
+                                                    data_types=new_data_types)
+        return new_array_manager_type
+
+    def delete_dimension(self, key):
+        new_data_types = self.data_types
+        del new_data_types[key]
+        new_array_manager_type = array_manager_type(name=name,
+                                                    data_types=new_data_types)
+        return new_array_manager_type
 
     def __getitem__(self, subscript):
         shape = (np.empty(self.shape)[subscript]).shape
-        new_point_cloud = type(self)(shape)
+        new_array_manager = type(self)(shape)
         for key in self.data_types:
-            setattr(new_point_cloud, key, getattr(self, key)[subscript])
-        return new_point_cloud
+            setattr(new_array_manager, key, getattr(self, key)[subscript])
+        return new_array_manager
 
     attribute_dict = dict(__init__=__init__,
                           __getitem__=__getitem__,
                           data_types=data_types,
-                          add_dimension=add_dimension)
+                          add_dimension=add_dimension,
+                          delete_dimension=delete_dimension)
 
     for dimension in data_types:
         attribute_dict[dimension] = property(fset=nd_array_setter(dimension, data_types[dimension]),
-                                             fget=nd_array_getter(dimension),
-                                             fdel=nd_array_deleter(dimension))
+                                             fget=nd_array_getter(dimension))
 
     return type(name + str(data_types), (object,), attribute_dict)
+
+
+def point_cloud_type(name, data_types):
+
+    def __init__(self, shape):
+        array_manager_type_data_types = array_manager_type("AM" + name, data_types)
+        self.__array_manager = array_manager_type_data_types(shape)
+        self.__dict__["__core_data_types"] = data_types
+        for item in self.__array_manager.data_types:
+            setattr(self, item, 0)
+
+    def __setattr__(self, key, value):
+        if key == "__array_manager":
+            self.__dict__["__array_manager"] = value
+        elif key in self.__array_manager.data_types or key:
+            setattr(self.__array_manager, key, value)
+        else:
+            pass
+
+    def __getattr__(self, key):
+        if key == "__array_manager":
+            return self.__dict__["__array_manager"]
+        if key == "__core_data_types":
+            return self.__dict__["__core_data_types"]
+        if key in self.__array_manager.data_types:
+            return getattr(self.__array_manager, key)
+        else:
+            pass
+
+    def __delattr__(self, key):
+        if key in self.__core_data_types:
+            raise AttributeError("You cannot delete a core attribute")
+        if key in self.__array_manager.data_types:
+            new_array_manager = self.__array_manager.delete_dimension(key)
+            delattr(new_array_manager, key)
+            self.__array_manager = new_array_manager
+        else:
+            pass
+
+    attribute_dict = dict(__init__=__init__,
+                          __setattr__=__setattr__,
+                          __delattr__=__delattr__,
+                          __getattr__=__getattr__)
+
+    return type(name, (), attribute_dict)
 
 
 RedHawkPointCloud = point_cloud_type(name="RedHawkPointCloud",
